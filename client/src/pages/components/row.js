691434927bac3f75fa341eba80bfa22dba7d8250
Geek-Deer <geekdeer@gmail.com>
2023-06-16 15:57:36 -0600

    whoops

--- client/src/pages/components/row.js
+++ client/src/pages/components/row.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 
 export default function Row({id , nombre, password, email,tareas,equipo}) {
+    if(equipo == 0){
+        var equipoN = "Por asignar"
+    }
     if(equipo == 1){
         var equipoN = "Comunicación"
       }
-      if(equipo == 2){
-        var equipoN = "Contenido"
-      }
-      if(equipo == 3){
-        var equipoN = "Contenido"
+    if(equipo == 2){
+    var equipoN = "Contenido"
+    }
+    if(equipo == 3){
+    var equipoN = "Contenido"
       }
     return (
 
